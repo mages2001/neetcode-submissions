788d25b638238748d46724f880c88c05d1da0bd2
maheswaran <97970475+mages2001@users.noreply.github.com>
2026-06-28 11:31:47 +0530

    Add: is-palindrome - submission-9

---
+++ Data Structures & Algorithms/is-palindrome/submission-9.js
@@ -0,0 +1,18 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isPalindrome(s) {
+        let s1 = s.toLowerCase().replace(/[^a-z0-9]/g, "");
+        let s2 = "";
+
+        for (let i = s1.length - 1; i >= 0; i--) {
+            s2 += s1[i];
+        }
+
+        console.log(s1, s2)
+        if (s1 === s2) return true;
+        else return false;
+    }
+}
